0108079976eec0a531f118801d19b46d507e3025
asviridov <asviridov@458ef540-594c-aa4e-96f3-e55b0777babc>
2018-12-14 10:53:33 +0000

    cf EGISZREMD-351 2.1-SNAPSHOT Обработка ошибок отсутствия файлов.
    git-svn-id: https://svn.i-novus.ru/svn/common/Common/file-storage/trunk@132714 458ef540-594c-aa4e-96f3-e55b0777babc

--- file-storage-api/src/main/java/ru/i_novus/common/file/storage/api/exception/EmptyFileException.java
+++ file-storage-api/src/main/java/ru/i_novus/common/file/storage/api/exception/EmptyFileException.java
@@ -1,8 +1,5 @@
 package ru.i_novus.common.file.storage.api.exception;
 
-import javax.ejb.ApplicationException;
-
-@ApplicationException
 public class EmptyFileException extends RuntimeException {
     public EmptyFileException() {
         super();
--- file-storage-api/src/main/java/ru/i_novus/common/file/storage/api/exception/NotFoundException.java
+++ file-storage-api/src/main/java/ru/i_novus/common/file/storage/api/exception/NotFoundException.java
@@ -1,8 +1,5 @@
 package ru.i_novus.common.file.storage.api.exception;
 
-import javax.ejb.ApplicationException;
-
-@ApplicationException
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
         super();
